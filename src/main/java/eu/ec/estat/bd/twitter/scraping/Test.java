36f8efe3492c49703319aaa258fbf53df398f755
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-08-31 14:43:03 +0200

    twitter test

---
+++ src/main/java/eu/ec/estat/bd/twitter/scraping/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package eu.ec.estat.bd.twitter.scraping;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Test {
+	//See: https://dev.twitter.com/rest/public
+	//https://dev.twitter.com/overview/general/things-every-developer-should-know
+	//https://dev.twitter.com/rest/public/rate-limiting
+
+	//??? https://dev.twitter.com/rest/reference/get/search/tweets
+	public static void main(String[] args) {
+	}
+
+}
