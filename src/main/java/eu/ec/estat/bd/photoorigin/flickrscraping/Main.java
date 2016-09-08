dde251c18db4abe834eef4969a48c710ac384e12
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-09-08 19:21:40 +0200

    0

--- src/main/java/eu/ec/estat/bd/photoorigin/flickrscraping/Main.java
+++ src/main/java/eu/ec/estat/bd/photoorigin/flickrscraping/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
 		int radius = 8;
 		PhotoSearch ps = new PhotoSearch(2000, 1, 2016, 12, "lat", "43.695949", "lon", "7.271413", "radius", ""+radius);
-		ps.getAndSaveWithGenericScheduler("out/flickr/", "nice_"+radius+"km_"+"2000-1"+".txt", true);
+		ps.getAndSaveWithGenericScheduler("out/flickr/", "nice_"+radius+"km_"+"2000-01"+".txt", true);
 
 		//PhotoSearch.getLocationInfo("out/flickr/nice___.txt", "out/flickr/", "locations.txt");
 
