19052f993f7cef4eedba94ec20e4e9e77f12dc45
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-09-22 17:30:35 +0200

    azure test

---
+++ src/main/java/eu/ec/estat/bd/azure/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package eu.ec.estat.bd.azure;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Test {
+	//https://azure.microsoft.com/en-us/documentation/articles/storage-java-how-to-use-blob-storage/
+	//https://github.com/azure/azure-storage-java
+	//http://azure.github.io/azure-storage-java/
+
+	public static void main(String[] args) {
+
+	}
+
+}
